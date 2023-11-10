e27e4afb53a4f71d112b7f33f084eda035843c2a
Ignacio Puglisi <puglisi.ignacio@gmail.com>
2023-11-09 23:45:21 -0300

    cleaning code

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,22 +1,22 @@
 import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
-// Define el esquema del carrito
+
 const cartSchema = new mongoose.Schema({
   products: [
     {
       product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
       quantity: Number,
     },
-  ], // Referencia al modelo Producto
+  ],
   total: Number,
-  // Otras propiedades del carrito, si es necesario
+
 });
 
-// Agregar el plugin de paginación al esquema del carrito
+
 cartSchema.plugin(mongoosePaginate);
 
-// Crear el modelo del carrito
+
 const Cart = mongoose.model("Cart", cartSchema);
 
 export default Cart;
